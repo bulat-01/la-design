08fb62cda49e07eca72fa017e67ce1bc72c6b1f6
bulat-01 <bulat.dzeytov@internet.ru>
2024-11-05 21:33:00 +0300

    task-15 Добавил функционал табам и при загрузке страницы отображается количество изображений в портфолио над табом

--- assets/js/home.js
+++ assets/js/home.js
@@ -1,21 +1,51 @@
-const arrowDown = document.querySelector('.arrow-bottom__img');
-const targetBlock = document.querySelector('.portfolio');
-const buttonToTop = document.querySelector('.button-to-top');
 const headerElem = document.querySelector('.header');
+const arrowDown = document.querySelector('.arrow-bottom__img');
+const buttonToTop = document.querySelector('.button-to-top');
+const targetBlock = document.querySelector('.portfolio');
+const tabsButtons = document.querySelectorAll('.tabs__tab');
 
-arrowDown.addEventListener('click', function(event) {
+arrowDown.addEventListener('click', (event) => {
     targetBlock.scrollIntoView({
         behavior: "smooth",
         block: "start",
       });
 })
 
-buttonToTop.addEventListener('click', function(event) {
+buttonToTop.addEventListener('click', (event) => {
   headerElem.scrollIntoView({
         behavior: "smooth",
         block: "start"
       });
 })
 
+document.addEventListener('DOMContentLoaded', () => {
+  tabsButtons.forEach((tabButton) => {
+    const tabContent = document.getElementById(tabButton.dataset.tab);
+    tabButton.children[0].textContent = tabContent.children.length;
+  })
+})
 
+tabsButtons.forEach((tabButton) => {
+  tabButton.addEventListener('click', (event) => {
+    const tabButtonActive = document.querySelector('.active');
+    console.log(tabButtonActive);
+    if (tabButtonActive) {
+      tabButtonActive.classList.remove('active');
+    }
+    
+    const tabContentActive = document.getElementById(tabButton.dataset.tab);
+    
+    tabContentActive.classList.add('active');
+
+    const line = document.querySelector('.tabs__black-line');
+    
+    if(event.target.tagName == 'DIV') {
+      line.style.width = event.target.offsetWidth - event.target.children[0].offsetWidth - 5  + 'px';
+      line.style.left = event.target.offsetLeft   + 'px'; 
+    } else if(event.target.tagName == 'SPAN') {
+      line.style.width = event.target.parentElement.offsetWidth -  event.target.offsetWidth - 5 + 'px';
+      line.style.left = event.target.parentElement.offsetLeft  + 'px'; 
+    }
+  })
+})
 
