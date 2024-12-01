a49862b9e8497dcf4de5ab515410734c0d13e83b
bulat-01 <bulat.dzeytov@internet.ru>
2024-12-01 13:42:51 +0300

    добавлен функционал табов и кнопки показать еще на страницу portfolio

--- assets/js/home.js
+++ assets/js/home.js
@@ -2,15 +2,8 @@ const headerElem = document.querySelector('.header');
 const arrowDown = document.querySelector('.arrow-bottom__img');
 const buttonToTop = document.querySelector('.button-to-top');
 const targetBlock = document.querySelector('.portfolio');
-const tabsButtons = document.querySelectorAll('.tabs__tab');
 const benefitsArrows = document.querySelectorAll('.benefits__arrow');
 const benefitsItemDescription = document.querySelectorAll('.benefits__item-description');
-const AllWorks = document.querySelector('.list__all-works');
-const AllWorksItems = Array.from(AllWorks.children);
-let currentWorksItems = 10;
-let hiddenWorksItems;
-let countRows = 4;
-const btnShowMore = document.querySelector('.portfolio__button');
 
 arrowDown.addEventListener('click', (event) => {
     targetBlock.scrollIntoView({
@@ -35,58 +28,3 @@ benefitsArrows.forEach((arrow) => {
     })
 })
 
-document.addEventListener('DOMContentLoaded', () => {
-  tabsButtons.forEach((tabButton) => {
-    const tabContent = document.getElementById(tabButton.dataset.tab);
-    tabButton.children[0].textContent = tabContent.children.length;
-  })
-})
-
-tabsButtons.forEach((tabButton) => {
-  tabButton.addEventListener('click', (event) => {
-    const tabButtonActive = document.querySelector('.active');
-    const tabContentActive = document.getElementById(tabButton.dataset.tab);
-    const line = document.querySelector('.tabs__black-line');
-
-    if (tabButtonActive) {
-      tabButtonActive.classList.remove('active');
-    }
-    
-    if (tabContentActive.classList.contains('list__all-works')) {
-      btnShowMore.style.display = 'flex';
-    } else {
-      btnShowMore.style.display = 'none';
-    }
-    
-    tabContentActive.classList.add('active');
-
-    line.style.width = event.currentTarget.offsetWidth - event.currentTarget.children[0].offsetWidth - 5  + 'px';
-    line.style.left = event.currentTarget.offsetLeft + 'px'; 
-  })
-})
-
-btnShowMore.addEventListener('click', (event) => {
-  if (btnShowMore.firstElementChild.textContent == 'Показать еще работы') {
-    hiddenWorksItems = AllWorksItems.slice(currentWorksItems, currentWorksItems + 7);
-    currentWorksItems += 7;
-    countRows += 3;
-    
-    hiddenWorksItems.forEach((item) => {
-      item.style.display = 'block';
-    })
-     
-    if (currentWorksItems == AllWorksItems.length) {
-      btnShowMore.firstElementChild.textContent = 'Свернуть';
-    }
-    
-  } else {
-      currentWorksItems = 10;
-      countRows = 4;
-
-      AllWorksItems.slice(10).forEach((item) => {
-        item.style.display = 'none';
-      })
-    
-      btnShowMore.firstElementChild.textContent = 'Показать еще работы';
-  }
-})
